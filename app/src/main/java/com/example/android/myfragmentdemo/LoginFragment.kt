b1ee44880784f3a9d98dfb292a0409c23f7da097
cch25 <Chim0205123_>
2021-07-18 23:26:56 +0800

    Initial commit

---
+++ app/src/main/java/com/example/android/myfragmentdemo/LoginFragment.kt
@@ -0,0 +1,41 @@
+package com.example.android.myfragmentdemo
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
+import com.example.android.myfragmentdemo.databinding.FragmentLoginBinding
+
+
+class LoginFragment : Fragment() {
+
+    private lateinit var binding: FragmentLoginBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        binding =  DataBindingUtil.inflate(inflater, R.layout.fragment_login, container, false)
+
+        binding.btnLogin.setOnClickListener(){
+            val userName : String = binding.tvUserName.text.toString()
+            val password : String = binding.tvPassword.text.toString()
+
+            if(userName == "abc" && password == "123"){
+
+                Navigation.findNavController(it).navigate(R.id.action_loginFragment2_to_questionFragment)
+            }else{
+                Toast.makeText(context, "Invalid username and password", Toast.LENGTH_LONG).show()
+
+            }
+        }
+
+        return binding.root
+    }
+
+}
---
+++ app/src/main/java/com/example/android/myfragmentdemo/Question.kt
@@ -0,0 +1,7 @@
+package com.example.android.myfragmentdemo
+
+data class Question(
+
+    var text: String,
+    var answers: List<String>
+)
